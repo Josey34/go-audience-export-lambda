00b9c09c7f9e08a9a2de15d5789b5b30d78b0295
Josey34 <joseytakesan1@gmail.com>
2026-05-20 21:52:11 +0700

    milestone 4: feat: add entity for audience row

---
+++ modules/entity/audience_row.go
@@ -0,0 +1,10 @@
+package entity
+
+type AudienceRow struct {
+	ProjectID       string
+	RetailerCode    string
+	RetailerName    string
+	ProductFlagging string
+	ProductName     string
+	ProductPrice    float64
+}
